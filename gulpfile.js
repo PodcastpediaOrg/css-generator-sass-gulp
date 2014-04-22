71996e1ea1dff12c7750df4a1d28aa3d590b1cfb
amacoder <ama@codingpedia.org>
2014-04-22 13:39:43 +0200

    added hyperlinks to gulp/gulp-plugins in the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 //Gulp plugin
-var gulp = require("gulp")
-        util = require("gulp-util"),
-        sass = require("gulp-sass"),
-        autoprefixer = require('gulp-autoprefixer'),
-        minifycss = require('gulp-minify-css'),
-        rename = require('gulp-rename'),
+var gulp = require("gulp")//http://gulpjs.com/
+        util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
+        sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
+        autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
+        minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
+        rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
         log = util.log;
  
 var sassFiles = "src/sass/**/*.scss";
