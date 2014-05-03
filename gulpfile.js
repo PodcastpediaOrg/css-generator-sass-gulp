9ee4eb9c54d4c3947bd87a677d3b216066b6e9e5
amacoder <ama@codingpedia.org>
2014-05-03 21:48:38 +0200

    Corrected missing comma

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 //Gulp plugin
-var gulp = require("gulp")//http://gulpjs.com/
-        util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
-        sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
-        autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
-        minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
-        rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
-        log = util.log;
+var gulp = require("gulp"),//http://gulpjs.com/
+		util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
+		sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
+		autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
+		minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
+		rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
+		log = util.log;
  
 var sassFiles = "src/sass/**/*.scss";
 
